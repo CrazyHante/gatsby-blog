e13411b8c85433beb35471de255aa6b9cb362e57
yuhantao <1597006187@qq.com>
2018-10-23 10:07:40 +0800

    init

---
+++ content/meta/config.js
@@ -0,0 +1,32 @@
+const colors = require("../../src/styles/colors");
+
+module.exports = {
+  siteTitle: "PersonalBlog - a blog starter for GatsbyJS", // <title>
+  shortSiteTitle: "PersonalBlog GatsbyJS Starter", // <title> ending for posts and pages
+  siteDescription: "PersonalBlog is a GatsbyJS starter.",
+  siteUrl: "https://gatsby-starter-personal-blog.greglobinski.com",
+  pathPrefix: "",
+  siteImage: "head.jpg",
+  siteLanguage: "en",
+  // author
+  authorName: "greg lobinski",
+  authorTwitterAccount: "greglobinski",
+  // info
+  infoTitle: "于瀚韬",
+  infoTitleNote: "个人博客",
+  // manifest.json
+  manifestName: "PersonalBlog - a blog starter for GatsbyJS",
+  manifestShortName: "PersonalBlog", // max 12 characters
+  manifestStartUrl: "/",
+  manifestBackgroundColor: colors.background,
+  manifestThemeColor: colors.background,
+  manifestDisplay: "standalone",
+  // contact
+  contactEmail: "john@doe.com",
+  // social
+  authorSocialLinks: [
+    { name: "github", url: "https://github.com/greglobinski" },
+    { name: "twitter", url: "https://twitter.com/greglobinski" },
+    { name: "facebook", url: "http://facebook.com/greglobinski" }
+  ]
+};
---
+++ content/posts/2018-10-22--test/index.md
@@ -0,0 +1,22 @@
+---
+title: 博客MD测试
+subTitle: 副标题
+category: "java"
+cover: 1.jpg
+---
+
+```java
+public static void main(String[] args){
+    System.out.println("博客测试")
+}
+```
+
+
+
+> 1.测试博客
+>
+> 2.格式正确
+
+
+
+![lion](./lion.jpg)
